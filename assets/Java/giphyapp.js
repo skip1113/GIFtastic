8345a6fba18d27b89a0638d8841870e55531f3e8
Skip Khamvongsa <skipk95@gmail.com>
2020-03-07 14:19:23 -0700

    fixed response and results to correct url to find gifs

--- assets/Java/giphyapp.js
+++ assets/Java/giphyapp.js
@@ -1,53 +1,55 @@
 //Array of shows for gifs
-var memes = ["trending", "the simpsons", "spongebob", "king of the hill", "south park", "bobs burger", "family guy", "tom and jerry"];
+var memes = ["the simpsons", "spongebob", "king of the hill", "south park", "bobs burger", "family guy", "tom and jerry"];
 //API test
 // var memes = $(this).attr("data-name");
 //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=simpsons&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
-    
-    
-        
-    //ajax call for giphy url
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(results) {
-    //     console.log(results.data);
-    // })
-//gif ,rating display
-function displayGif() {
 
-    var memes = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + memes + "&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
-        
+
+
+// //ajax call for giphy url
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(results) {
+//     console.log("rating " + results[0].rating);
+//})
+// gif ,rating display
+function displayGif() {
+    var limit = 10;
+    // var memes = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + memes + "&limit=" + limit + "&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
+
     //ajax call for giphy url
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         var results = response.data;
-
+        console.log(results);
+        // var imgUrl = response[0].url;
         for (var i = 0; i < results.length; i++) {
-            //take action if gif is not rated r
-            if (results[i].rating !== "r") {
-                //create gif div
-                var gifDiv = $("<div>");
-                //store gif ratings
-                var rating = results.rating;
-                //creating p tag with rating and rating results
-                var p = $("<p>").text("Rating: " + rating);
-                //creating image tag
-                var gifImage = $("<img>");
-                //give gifimage src attribute for image
-                gifImage.attr("src", results[i].image.fixed_height.url);
-                //append image and rating to new div created
-                gifDiv.append(gifImage);
-                gifDiv.append(p);
-                //prepend image and rating to html
-                $("#gifs-here").prepend(gifDiv);
-            }
-        }
+            
+            //create gif div
+            var gifDiv = $("<div>");
+            //store gif ratings
+            var rating = results[0].rating;
+            //creating p tag with rating and rating results
+            var p = $("<p>").text("Rating: " + rating);
+            //creating image tag
+            var gifImage = $("<img>");
+            //give gifimage src attribute for image
+            gifImage.attr("src", results[i].images.fixed_height_small.url);
+            //append image and rating to new div created
+            gifDiv.append(gifImage);
+            gifDiv.append(p);
+            //prepend image and rating to html
+            $("#gifs-here").prepend(gifDiv);
+        }    
+
+
     });
 }
+displayGif();
 //function to display meme buttons
 function displayButton() {
     $("#button-view").empty();
@@ -60,4 +62,13 @@ function displayButton() {
         $("#button-view").append(a);
     }
 }
+$("#add-gif").on("click", function (event) {
+    event.preventDefult();
+    var meme = $("#movie-input").val().trim();
+    memes.push(meme);
+    displayButton();
+    displayGif();
+})
+//add event click function on all gif-btn
+$(document).on("click", ".meme-btn", displayGif);
 displayButton();
