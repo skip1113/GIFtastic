31543278d0a0fbec580be95b8ccd4e6523bfece2
Skip Khamvongsa <skipk95@gmail.com>
2020-03-09 17:29:25 -0600

    fixed button function, added classes and attributes to display function

--- assets/Java/giphyapp.js
+++ assets/Java/giphyapp.js
@@ -1,5 +1,5 @@
 //Array of shows for gifs
-var memes = ["the simpsons", "spongebob", "king of the hill", "south park", "bobs burger", "family guy", "tom and jerry"];
+var memes = ["Simpsons", "Spongebob", "King of the hill", "South park", "Bobs burger", "Family guy", "Tom and jerry"];
 //API test
 // var memes = $(this).attr("data-name");
 //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=simpsons&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
@@ -16,29 +16,35 @@ var memes = ["the simpsons", "spongebob", "king of the hill", "south park", "bob
 // gif ,rating display
 function displayGif() {
     var limit = 10;
-    // var memes = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + memes + "&limit=" + limit + "&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
+    var searchMemes = $(this).attr("data-name");
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchMemes}&limit=${limit}&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU`;
 
     //ajax call for giphy url
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
         var results = response.data;
         console.log(results);
         // var imgUrl = response[0].url;
         for (var i = 0; i < results.length; i++) {
             
             //create gif div
-            var gifDiv = $("<div>");
+            var gifDiv = $("<div id='gifId'>").addClass("mx-auto text-center");
             //store gif ratings
-            var rating = results[i].rating;
+            var ratings = results[i].rating;
             //creating p tag with rating and rating results
-            var p = $("<p>").text("Rating: " + rating);
+            var p = $("<p>").text("Rated: " + ratings);
+            var headClick = $("<p>").text("Click to Animate!");
             //creating image tag
-            var gifImage = $("<img>");
+            var gifImage = $("<img>").addClass("jiff");
             //give gifimage src attribute for image
-            gifImage.attr("src", results[i].images.fixed_height_small.url);
+            gifImage.attr("src", results[i].images.fixed_height_still.url).append(gifImage);
+            //give gifImage attribute of stop and animate
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url).append(gifImage);
+            gifImage.attr("data-animate", results[i].images.fixed_height_small.url).append(gifImage);
+            gifDiv.append(headClick);
+            gifImage.attr("data-state", "still").append(gifImage);
             //append image and rating to new div created
             gifDiv.append(gifImage);
             gifDiv.append(p);
@@ -52,23 +58,34 @@ function displayGif() {
 // displayGif();
 //function to display meme buttons
 function displayButton() {
-    $("#button-view").empty();
+    $("#buttons-view").empty();
 
     for (var i = 0; i < memes.length; i++) {
         var a = $("<button>");
-        a.addClass("meme-btn");
+        a.addClass("gif-btn btn btn-primary");
         a.attr("data-name", memes[i]);
         a.text(memes[i]);
-        $("#button-view").append(a);
+        $("#buttons-view").append(a);
     }
 }
-$("#add-gif").on("click", function (event) {
-    event.preventDefult();
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
     var meme = $("#gif-input").val().trim();
-    meme.push(memes);
+    memes.push(meme);
     displayButton();
-    displayGif();
+    // displayGif();
 })
 //add event click function on all gif-btn
-$(document).on("click", ".meme-btn", displayGif);
+$(document).on("click", ".gif-btn", displayGif);
+//click event to animate gifs
+$(document).on("click", ".jiff", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
 displayButton();
