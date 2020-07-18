150f7b5695e172777b43571ebd4b37603ba1017c
Skip Khamvongsa <skipk95@gmail.com>
2020-07-18 13:51:52 -0600

    Replaced vars with const and lets

--- assets/Java/giphyapp.js
+++ assets/Java/giphyapp.js
@@ -1,35 +1,35 @@
 //Array of shows for gifs
-var memes = ["Simpsons", "Spongebob", "King of the hill", "South park", "Bobs burger", "Family guy", "Tom and jerry"];
+const memes = ["Simpsons", "Spongebob", "King of the hill", "South park", "Bobs burger", "Family guy", "Tom and jerry"];
 
 function displayGif() {
-    var limit = 10;
-    var searchMemes = $(this).attr("data-name");
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchMemes}&limit=${limit}&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU`;
-
+    // var limit = 10;
+    const searchMemes = $(this).attr("data-name");
+    // var queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchMemes}&limit=${limit}&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU`;
+    const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchMemes + "&limit=10&api_key=LS1HNxaxg8yQf68z10qhwgr4cY9m12FU";
     //ajax call for giphy url
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var results = response.data;
+        let results = response.data;
         console.log(results);
-        // var imgUrl = response[0].url;
+        // let imgUrl = response[0].url;
         displayFunc(response);
     });
 }
 function displayFunc(response) {
-    var results = response.data;
-    for (var i = 0; i < results.length; i++) {
+    let results = response.data;
+    for (let i = 0; i < results.length; i++) {
             
         //create gif div
-        var gifDiv = $("<div id='gifId'>").addClass("mx-auto text-center");
+        let gifDiv = $("<div id='gifId'>").addClass("mx-auto text-center");
         //store gif ratings
-        var ratings = results[i].rating;
+        let ratings = results[i].rating;
         //creating p tag with rating and rating results
-        var p = $("<p>").text("Rated: " + ratings);
-        var headClick = $("<p>").text("Click to Animate!");
+        let p = $("<p>").text("Rated: " + ratings);
+        let headClick = $("<p>").text("Click to Animate!");
         //creating image tag
-        var gifImage = $("<img>").addClass("jiff");
+        let gifImage = $("<img>").addClass("jiff");
         //give gifimage src attribute for image
         gifImage.attr("src", results[i].images.fixed_height_still.url).append(gifImage);
         //give gifImage attribute of stop and animate
@@ -49,8 +49,8 @@ function displayFunc(response) {
 function displayButton() {
     $("#buttons-view").empty();
 
-    for (var i = 0; i < memes.length; i++) {
-        var a = $("<button>");
+    for (let i = 0; i < memes.length; i++) {
+        let a = $("<button>");
         a.addClass("gif-btn btn btn-info");
         a.attr("data-name", memes[i]);
         a.text(memes[i]);
@@ -59,7 +59,7 @@ function displayButton() {
 }
 $("#add-gif").on("click", function(event) {
     event.preventDefault();
-    var meme = $("#gif-input").val().trim();
+    let meme = $("#gif-input").val().trim();
     memes.push(meme);
     displayButton();
     // displayGif();
@@ -68,7 +68,7 @@ $("#add-gif").on("click", function(event) {
 $(document).on("click", ".gif-btn", displayGif);
 //click event to animate gifs
 $(document).on("click", ".jiff", function() {
-    var state = $(this).attr("data-state");
+    let state = $(this).attr("data-state");
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
